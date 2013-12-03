329282f71b726168eded2a4925901e0deaacb830
dpenfoldbrown <dpenfoldbrown@gmail.com>
2013-12-03 00:11:47 -0500

    Split week09 into two parts

--- python/week09-p2-solns.py
+++ python/week09-p2-solns.py
@@ -1,53 +1,9 @@
 """
-Programming Group week 09 - Reading raw tweets from file (such as those stored from a
-streaming collection)
-
-Tweets  -   https://dev.twitter.com/docs/platform-objects/tweets
+Programming Group week09 part 2 - solutions to suggestions
 
 @auth dpb
 @date 12/02/2013
 """
-
-import simplejson as json
-from json_util import ConcatJSONDecoder
-
-
-# STEP 1: Read tweets in from file and store in a list of tweets
-# There's a trick here... just use this ConcatJSONDecoder tool (can also do it line-by-line)
-tweetfile = "UkraineGeoTweets.json.txt"
-handle = open(tweetfile)
-tweets = json.loads(handle.read(), cls=ConcatJSONDecoder)
-handle.close()
-
-# Line-by-line looks like this:
-handle = open(tweetfile)
-tweets = []
-for line in handle:
-    tweet = json.loads(line.rstrip())
-    tweets.append(tweet)
-handle.close()
-
-
-# Super secret answers below. We want to:
-# 1 - Loop over all tweets
-# 2 - Check if each tweet is in fact a tweet
-# 3 - Check for Language, Place, and Geocoded data
-# 4 - Store each of Language occurrence, Country occurrence, and Coordinates
-# 5 - Write each of these to very simple CSV files
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # STEP 2: Do something interesting with the tweets
 # (Store language and country counts, an a list of GPS coords)
@@ -105,5 +61,3 @@ for c in coords:
 handle.close()
 
 # Make plot of language?
-
-
